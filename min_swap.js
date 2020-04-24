dd2263f671981609e10f5dec30f80d05b3c2c8a8
narajaon <rajaonarisonfabien@yahoo.fr>
2020-04-24 12:43:22 +0200

    update remove fluff

--- min_swap.js
+++ min_swap.js
@@ -1,29 +1,3 @@
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
 function dive(arr, i, found) {
     let count = 0;
     let cur = i;
@@ -54,18 +28,3 @@ function minimumSwaps(arr) {
 
     return count;
 }
-
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const n = parseInt(readLine(), 10);
-
-    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
-
-    const res = minimumSwaps(arr);
-
-    ws.write(res + '\n');
-
-    ws.end();
-}
-
