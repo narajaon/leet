ee3ab18e9f5d7ccfac7e45190100f3ad2fa0a770
narajaon <rajaonarisonfabien@yahoo.fr>
2020-04-29 19:52:41 +0200

    max min

---
+++ max_min.js
@@ -0,0 +1,16 @@
+// ihttps://www.hackerrank.com/challenges/angry-children/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=greedy-algorithms
+// Complete the maxMin function below.
+function maxMin(k, arr) {
+    arr.sort((a, b) => a - b)
+
+    let min = Infinity;
+    for (let i = 0; i < arr.length - 1; i++) {
+        const res = arr[i + k - 1] - arr[i]
+        if (res < min) {
+            min = res;
+        }
+    }
+
+    return min; 
+}
+
