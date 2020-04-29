e5afc182ec83d7d882622c04b9277d2a07f3a8da
Fabien RAJAONARISON <rajaonarisonfabien@yahoo.fr>
2020-04-29 14:55:18 +0200

    Update make_anagram.js

--- make_anagram.js
+++ make_anagram.js
@@ -1,19 +1,17 @@
 // https://www.hackerrank.com/challenges/ctci-making-anagrams/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
 // Complete the makeAnagram function below.
 function makeAnagram(a, b) {
-    const short = a.length <= b.length ? a : b;
-    const long = b.length >= a.length ? b : a;
     const found = {}
     let count = 0
 
-    for (let i = 0; i < short.length; i++) {
-        found[short[i]] = found[short[i]] ? found[short[i]] + 1 : 1; 
+    for (let i = 0; i < a.length; i++) {
+        found[a[i]] = found[a[i]] ? found[a[i]] + 1 : 1; 
     }
 
-    for (let i = 0; i < long.length; i++) {
-        if (found[long[i]] && found[long[i]] > 0) {
+    for (let i = 0; i < b.length; i++) {
+        if (found[b[i]] && found[b[i]] > 0) {
             count++
-            found[long[i]]--
+            found[b[i]]--
         } 
     }
 
