239ffd7c28e4fe9439a5069e8c9efd5c6f376e43
narajaon <rajaonarisonfabien@yahoo.fr>
2020-04-29 14:44:10 +0200

    anagram

---
+++ make_anagram.js
@@ -0,0 +1,12 @@
+https://www.hackerrank.com/challenges/ctci-making-anagrams/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
+// Complete the makeAnagram function below.
+function makeAnagram(a, b) {
+        const short = a.length <= b.length ? a : b;
+        const long = b.length >= a.length ? b : a;
+    …            found[long[i]]--
+            } 
+    }
+
+    return a.length + b.length - count * 2
+}
+
